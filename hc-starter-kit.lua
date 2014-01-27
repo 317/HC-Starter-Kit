2c107cfe52625ea4375d0b76e4591f957352b8f3
Thierno <t.rignoux@gmail.com>
2014-01-27 14:15:24 +0100

    Update hc-starter-kit.lua

--- hc-starter-kit.lua
+++ hc-starter-kit.lua
@@ -45,9 +45,9 @@ function PLUGIN:SetKitData()
   
   --Content definition of the premium kit
   self.kitsData["premium"] = {}
-  self.kitsData["basic"][1]={}
-  self.kitsData["basic"][1]["name"] = "Stone Hatchet"
-  self.kitsData["basic"][1]["amount"] = 1
+  self.kitsData["premium"][1]={}
+  self.kitsData["premium"][1]["name"] = "Stone Hatchet"
+  self.kitsData["premium"][1]["amount"] = 1
   
   self.kitsRawData = util.GetDataFile("hc-starter-kits")
   self.kitsRawData:SetText(json.encode(self.kitsData))
