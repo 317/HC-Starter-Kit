36b1dbfe4990608a8f7327d8ea88494fb784ee36
Thierno <t.rignoux@gmail.com>
2014-01-27 11:38:18 +0100

    Create hc-starter-kit.lua
    Create the lua script

---
+++ hc-starter-kit.lua
@@ -0,0 +1,48 @@
+--[[ *************** ]]--
+--[[ HC STARTER KIT - DiDay & 317 ]]--
+--[[ Thanks to the kits plugin from thomasfn ]]--
+--[[ *************** ]]--
+
+-- Define plugin variables
+PLUGIN.Title = "HC STARTER KIT"
+PLUGIN.Description = "Provide optimized starter kits"
+
+-- *******************************************
+-- PLUGIN:Init()
+-- Called when the plugin is initialised
+-- *******************************************
+function PLUGIN:Init()
+
+end
+
+-- *******************************************
+-- PLUGIN:OnSpawnPlayer()
+-- Called when the plugin is initialised
+-- *******************************************
+function PLUGIN:OnSpawnPlayer()
+
+end
+
+-- *******************************************
+-- PLUGIN:GiveKit()
+-- Function deciding which kit should be given
+-- *******************************************
+function PLUGIN:GiveKit(netuser, cmd, args )
+
+end
+
+-- *******************************************
+-- PLUGIN:GiveKit()
+-- Function which gives a premium kit
+-- *******************************************
+function PLUGIN:GivePremiumKit(netuser, cmd, args )
+
+end
+
+-- *******************************************
+-- PLUGIN:GiveKit()
+-- Function which give the basic kit
+-- *******************************************
+function PLUGIN:GiveBasicKit(netuser, cmd, args )
+
+end
